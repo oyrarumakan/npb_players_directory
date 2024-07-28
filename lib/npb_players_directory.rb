ba4f77fa12811b8f4987119795651ac4c22740b8
oyrarumakan <ryo19911030@hotmail.co.jp>
2024-07-28 17:35:20 +0900

    :rocket: initial commit

---
+++ lib/npb_players_directory.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "npb_players_directory/version"
+
+module NpbPlayersDirectory
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/npb_players_directory/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module NpbPlayersDirectory
+  VERSION = "0.1.0"
+end
